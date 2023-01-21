1053bb2cc67b40db37c24f83a86f1edbaf655f3b
Tom <13634684+dirtyformal@users.noreply.github.com>
2023-01-21 14:18:49 +1300

    Test ENV

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,11 @@
 import express from 'express';
+import {testEnvironmentVariable} from '../settings';
 
 // eslint-disable-next-line new-cap
 const indexRouter = express.Router();
 
 indexRouter.get('/', (req, res) =>
-  res.status(200).json({message: 'Welcome to Express API template'}),
+  res.status(200).json({message: testEnvironmentVariable}),
 );
 
 export default indexRouter;
