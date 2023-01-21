f08d0319cbea2ade453e6f57bff7026ad0bd1958
Tom <13634684+dirtyformal@users.noreply.github.com>
2023-01-21 16:20:23 +1300

    Update settings.js

--- src/settings.js
+++ src/settings.js
@@ -4,3 +4,4 @@ dotenv.config();
 
 export const testEnvironmentVariable = process.env.TEST_ENV_VARIABLE;
 export const connectionString = process.env.CONNECTION_STRING;
+export const localConnectionString = process.env.LOCAL_SECTOR_DB;
